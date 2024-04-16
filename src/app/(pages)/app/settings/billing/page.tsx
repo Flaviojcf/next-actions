0183a66be3de5315e29fee5dd0b6245b4eac0387
flavio <flavi@DESKTOP-USTCPSC>
2024-04-16 15:45:01 -0300

    refactor: Add dynamic values from db and stripe

--- src/app/(pages)/app/settings/billing/page.tsx
+++ src/app/(pages)/app/settings/billing/page.tsx
@@ -8,32 +8,45 @@ import {
   CardTitle,
 } from '@/components/ui/card'
 import { Progress } from '@/components/ui/progress'
+import { createCheckoutSessionAction } from './actions'
+import { auth } from '@/services/auth'
+import { getUserCurrentPlan } from '@/services/stripe'
 
-export default function Page() {
+export default async function Page() {
+  const session = await auth()
+  console.log(session?.user)
+  const plan = await getUserCurrentPlan(session?.user?.id as string)
+
   return (
-    <Card>
-      <CardHeader className="border-b border-border">
-        <CardTitle>Plano Atual</CardTitle>
-        <CardDescription>
-          Você está atualmente no [current_pan]. Ciclo de faturamento atual
-          [next_due_date]
-        </CardDescription>
-      </CardHeader>
-      <CardContent className="pt-6">
-        <div className="space-y-2">
-          <header className="flex items-center justify-between">
-            <span className="text-sm text-muted-foreground">1/5</span>
-            <span className="text-sm text-muted-foreground">20%</span>
-          </header>
-          <main>
-            <Progress value={20} />
-          </main>
-        </div>
-      </CardContent>
-      <CardFooter className="border-boder flex items-center justify-between border-t pt-6">
-        <span>Para um maior limite, assine o PRO</span>
-        <Button>Assinar o PRO</Button>
-      </CardFooter>
-    </Card>
+    <form action={createCheckoutSessionAction}>
+      <Card>
+        <CardHeader className="border-b border-border">
+          <CardTitle>Plano Atual</CardTitle>
+          <CardDescription>
+            Você está atualmente no{' '}
+            <span className="font-bold uppercase">{plan.name}</span>
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="pt-6">
+          <div className="space-y-2">
+            <header className="flex items-center justify-between">
+              <span className="text-sm text-muted-foreground">
+                {plan.quota.TASKS.current}/{plan.quota.TASKS.available}
+              </span>
+              <span className="text-sm text-muted-foreground">
+                {plan.quota.TASKS.usage}%
+              </span>
+            </header>
+            <main>
+              <Progress value={plan.quota.TASKS.usage} />
+            </main>
+          </div>
+        </CardContent>
+        <CardFooter className="border-boder flex items-center justify-between border-t pt-6">
+          <span>Para um maior limite, assine o PRO</span>
+          <Button type="submit">Assinar por R$5/ mês</Button>
+        </CardFooter>
+      </Card>
+    </form>
   )
 }
